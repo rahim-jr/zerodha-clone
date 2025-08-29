63a5e0c0b1bf47ee9cc1f330f73157ffcbb02fee
Md. Abdur Rahim <rahim.cse.diu@gmail.com>
2025-08-29 15:25:37 +0600

    modified Education section

--- src/landing page/home/Education.js
+++ src/landing page/home/Education.js
@@ -5,10 +5,12 @@ function Education() {
     <div className="container p-3">
       <div className="row p-5">
         <div className="col-6 p-5">
-         
+
         </div>
         <div className="col-6 p-5 mb-5">
-          
+          <h1 className='mb-3'>Free and open market education</h1>
+            <p>Varsity, the largest online stock market education book in the world covering everything from the basics to advanced trading.</p>
+
         </div>
       </div>
     </div>
